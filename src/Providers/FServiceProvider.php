44d87bccc8232077e6eb27f97954a30eb4222913
jongwoo Yoo <dbwhddn10@gmail.com>
2020-12-13 02:00:05 +0900

    change tab to 4 space in FServiceProvider.php

--- src/Providers/FServiceProvider.php
+++ src/Providers/FServiceProvider.php
@@ -14,20 +14,20 @@ class FServiceProvider extends ServiceProvider
     {
         Service::setResolverForNewCollection(function ($items=[]) {
 
-	        return new Collection($items);
-    	});
+            return new Collection($items);
+        });
         Service::setResolverForGetValidationErrors(function ($data=[], $ruleLists=[], $names=[]) {
 
-	        $factory = app(ValidationFactory::class);
-	        $factory->resolver(function ($tr, array $data, array $rules, array $messages, array $names)
-	        {
-	            return new Validator($tr, $data, $rules, $messages, $names);
-	        });
+            $factory = app(ValidationFactory::class);
+            $factory->resolver(function ($tr, array $data, array $rules, array $messages, array $names)
+            {
+                return new Validator($tr, $data, $rules, $messages, $names);
+            });
 
-	        $validator = $factory->make($data, $ruleLists, $messages=[], $names);
-	        $validator->passes();
+            $validator = $factory->make($data, $ruleLists, $messages=[], $names);
+            $validator->passes();
 
-	        return $validator->errors()->all();
-    	});
+            return $validator->errors()->all();
+        });
     }
 }
