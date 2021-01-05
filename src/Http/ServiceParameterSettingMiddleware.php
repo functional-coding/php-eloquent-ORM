b051f0e9fec6bcbb4d9f6de1e514f435c865e316
jongwoo Yoo <dbwhddn10@gmail.com>
2021-01-05 17:54:31 +0900

    :fix :error in ServiceParameterSettingMiddleware.php

--- src/Http/ServiceParameterSettingMiddleware.php
+++ src/Http/ServiceParameterSettingMiddleware.php
@@ -33,43 +33,43 @@ class ServiceParameterSettingMiddleware
             $names['token'] = 'header[authorization]';
         }
 
-        if ( array_key_exists('expands', $ruleLists) )
+        if ( $request->offsetExists('expands') )
         {
             $data['expands'] = Arr::get($request->all(), 'expands', '');
             $names['expands'] = '[expands]';
         }
 
-        if ( array_key_exists('fields', $ruleLists) )
+        if ( $request->offsetExists('fields') )
         {
             $data['fields'] = Arr::get($request->all(), 'fields', '');
             $names['fields'] = '[fields]';
         }
 
-        if ( array_key_exists('limit', $ruleLists) )
+        if ( $request->offsetExists('limit') )
         {
             $data['limit'] = Arr::get($request->all(), 'limit', '');
             $names['limit'] = '[limit]';
         }
 
-        if ( array_key_exists('id', $ruleLists) )
+        if ( $request->route('id') )
         {
             $data['id']  = $request->route('id') ? $request->route('id') : '';
             $names['id'] = $request->route('id') ? $request->route('id') : '';
         }
 
-        if ( array_key_exists('order_by', $ruleLists) )
+        if ( $request->offsetExists('order_by') )
         {
             $data['order_by'] = Arr::get($request->all(), 'order_by', '');
             $names['order_by'] = '[order_by]';
         }
 
-        if ( array_key_exists('cursor_id', $ruleLists) )
+        if ( $request->offsetExists('cursor_id') )
         {
             $data['cursor_id']  = Arr::get($request->all(), 'cursor_id', '');
             $names['cursor_id'] = '[cursor_id]';
         }
 
-        if ( array_key_exists('page', $ruleLists) )
+        if ( $request->offsetExists('page') )
         {
             $data['page']  = Arr::get($request->all(), 'page', '');
             $names['page'] = '[page]';
