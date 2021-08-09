045991de15fb613fa1e1d1562854324bd38977ce
jongwoo Yoo <dbwhddn10@gmail.com>
2021-08-09 15:36:07 +0900

    rename provider files and remove laravel global function usage

--- src/Providers/ModelRelationMapProvider.php
+++ src/Providers/ModelRelationMapProvider.php
@@ -4,31 +4,30 @@ namespace Dbwhddn10\FService\Illuminate\Providers;
 
 use ArrayIterator;
 use Illuminate\Database\Eloquent\Relations\Relation;
+use Illuminate\Container\Container;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
 use RecursiveDirectoryIterator;
 use RecursiveIteratorIterator;
 use RegexIterator;
 
-class MorphMapServiceProvider extends ServiceProvider
+class ModelRelationMapProvider extends ServiceProvider
 {
     public function register()
     {
-        $path  = base_path('app/Models');
+        $path  = Container::getInstance()->path('Models');
         $dir   = new RecursiveDirectoryIterator($path);
         $list  = new RecursiveIteratorIterator($dir);
         $files = new RegexIterator($list, '/.+\.php$/');
         $types = [];
 
-        foreach ($files as $file)
-        {
+        foreach ($files as $file) {
             require_once $file->getPathname();
         }
 
         $classes = new RegexIterator(new ArrayIterator(get_declared_classes()), '/App\\\Models/');
 
-        foreach ( $classes as $class )
-        {
+        foreach ($classes as $class) {
             $segs = explode('\\', $class);
             $key  = Str::snake(array_pop($segs));
 
--- src/Providers/ServiceValidationProvider.php
+++ src/Providers/ServiceValidationProvider.php
@@ -4,24 +4,23 @@ namespace Dbwhddn10\FService\Illuminate\Providers;
 
 use Dbwhddn10\FService\Service;
 use Dbwhddn10\FService\Illuminate\Validator;
+use Illuminate\Container\Container;
 use Illuminate\Support\Collection;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Validation\Factory as ValidationFactory;
 
-class FServiceProvider extends ServiceProvider
+class ServiceValidationProvider extends ServiceProvider
 {
     public function boot()
     {
-        Service::setResolverForNewCollection(function ($items=[]) {
-
-            return new Collection($items);
-        });
         Service::setResolverForGetValidationErrors(function ($data=[], $ruleLists=[], $names=[]) {
-
-            $factory = app(ValidationFactory::class);
-            $factory->resolver(function ($tr, array $data, array $rules, array $messages, array $names)
-            {
-                return new Validator($tr, $data, $rules, $messages, $names);
+            $app = Container::getInstance();
+            $factory = $app->make(ValidationFactory::class);
+            $factory->resolver(function ($tr, array $data, array $rules, array $messages, array $names) {
+                return $app->make(
+                    Validator::class,
+                    compact('tr', 'data', 'rules', 'messages', 'names')
+                );
             });
 
             $validator = $factory->make($data, $ruleLists, $messages=[], $names);
