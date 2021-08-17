c6517cc6ec4bee097f157dc48bb0aa24ea2aeeeb
jongwoo Yoo <dbwhddn10@gmail.com>
2021-08-17 09:32:13 +0900

    add null, not_null, null_if, not_null_if validator

--- src/Validation/Illuminate/lang/en.php
+++ src/Validation/Illuminate/lang/en.php
@@ -1,3 +1,8 @@
 <?php
 
-return [];
+return [
+    'null' => ':attribute must not exist.',
+    'null_if' => ':attribute must not exist.',
+    'not_null' => ':attribute must exist.',
+    'not_null_if' => ':attribute must exist.',
+];
--- src/Validation/Illuminate/validators.php
+++ src/Validation/Illuminate/validators.php
@@ -1,3 +1,39 @@
 <?php
 
-return [];
+return [
+    'not_null' => function ($attribute, $value, $parameters, $validator) {
+        return !$validator->validateNull($attribute, $value);
+    },
+
+    'not_null_if' => function ($attribute, $value, $parameters, $validator) {
+        $validator->requireParameterCount(2, $parameters, 'not_null_if');
+
+        $ifValue = $validator->getValue($parameters[0]);
+
+        if ($ifValue != $parameters[1]) {
+            return true;
+        }
+
+        return $validator->validateNotNull($attribute, $value);
+    },
+
+    'null' => function ($attribute, $value, $parameters, $validator) {
+        if (is_null($value)) {
+            return true;
+        }
+
+        return false;
+    },
+
+    'null_if' => function ($attribute, $value, $parameters, $validator) {
+        $validator->requireParameterCount(2, $parameters, 'null_if');
+
+        $ifValue = $validator->getValue($parameters[0]);
+
+        if ($ifValue != $parameters[1]) {
+            return true;
+        }
+
+        return $validator->validateNull($attribute, $value);
+    },
+];
