f7b761775a05ac732e92b550dc6493a3d02f03fa
jongwoo Yoo <dbwhddn10@gmail.com>
2021-08-19 23:13:12 +0900

    delete null_if, not_null_if rule

--- src/Validation/Illuminate/lang/en.php
+++ src/Validation/Illuminate/lang/en.php
@@ -1,8 +1,6 @@
 <?php
 
 return [
-    'null' => ':attribute must not exist.',
-    'null_if' => ':attribute must not exist.',
     'not_null' => ':attribute must exist.',
-    'not_null_if' => ':attribute must exist.',
+    'null' => ':attribute must not exist.',
 ];
--- src/Validation/Illuminate/validators.php
+++ src/Validation/Illuminate/validators.php
@@ -5,35 +5,11 @@ return [
         return !$this['null']($attribute, $value, [], $validator);
     },
 
-    'not_null_if' => function ($attribute, $value, $parameters, $validator) {
-        $validator->requireParameterCount(2, $parameters, 'not_null_if');
-
-        $ifValue = $validator->getValue($parameters[0]);
-
-        if ($ifValue != $parameters[1]) {
-            return true;
-        }
-
-        return $this['not_null']($attribute, $value, [], $validator);
-    },
-
     'null' => function ($attribute, $value, $parameters, $validator) {
         if (is_null($value)) {
             return true;
         }
 
         return false;
-    },
-
-    'null_if' => function ($attribute, $value, $parameters, $validator) {
-        $validator->requireParameterCount(2, $parameters, 'null_if');
-
-        $ifValue = $validator->getValue($parameters[0]);
-
-        if ($ifValue != $parameters[1]) {
-            return true;
-        }
-
-        return $this['null']($attribute, $value, [], $validator);
     },
 ];
