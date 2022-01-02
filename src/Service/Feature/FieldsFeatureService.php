ce90151d5406396f6ef3afc07beed34038cd9c2b
jongwoo Yoo <dbwhddn10@gmail.com>
2022-01-03 04:39:12 +0900

    fix: model appends property problem when get data

--- src/Service/Feature/FieldsFeatureService.php
+++ src/Service/Feature/FieldsFeatureService.php
@@ -3,6 +3,7 @@
 namespace FunctionalCoding\ORM\Eloquent\Service\Feature;
 
 use FunctionalCoding\Service;
+use Illuminate\Database\Eloquent\Model;
 
 class FieldsFeatureService extends Service
 {
@@ -18,12 +19,33 @@ class FieldsFeatureService extends Service
         return [
             'query.fields' => function ($availableFields, $fields = '', $query) {
                 $fields = $fields ? preg_split('/\s*,\s*/', $fields) : $availableFields;
+                $modelClass = get_class($query->getModel());
+                $model = new $modelClass;
+                $fields = array_diff($fields, array_keys($model->toArray()));
 
                 foreach ($fields as $i => $field) {
-                    $fields[$i] = $query->getModel()->getTable().'.'.$field;
+                    $fields[$i] = $model->getTable().'.'.$field;
                 }
 
                 $query->select($fields);
+            },
+
+            'query.fields:after_commit' => function ($availableFields, $fields = '', $modelClass, $result) {
+                $fields = $fields ? preg_split('/\s*,\s*/', $fields) : $availableFields;
+                $model = new $modelClass;
+                $fields = array_intersect($fields, array_keys($model->toArray()));
+
+                if ($result instanceof Model) {
+                    $collection = $result->newCollection([$result]);
+                } else {
+                    $collection = $result;
+                }
+
+                foreach ($collection as $model) {
+                    foreach ($fields as $field) {
+                        $model[$field] = $model->{$field};
+                    }
+                }
             },
         ];
     }
@@ -34,7 +56,11 @@ class FieldsFeatureService extends Service
             'available_fields' => function ($modelClass) {
                 $model = new $modelClass();
 
-                return array_merge($model->getFillable(), $model->getGuarded());
+                return array_merge(
+                    array_keys($model->toArray()), // appends
+                    $model->getFillable(),
+                    $model->getGuarded(),
+                );
             },
         ];
     }
